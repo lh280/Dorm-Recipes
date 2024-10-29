5fc0e52def9054d64264a992bfab2d0644a371df
= <=>
2024-10-28 23:52:54 -0400

    implemented basic static home page

---
+++ src/components/Section.js
@@ -0,0 +1,26 @@
+export default function Section(title, openRecipe) {
+  const tempRecipeIds = ["id1", "id2"];
+
+  const recipes = tempRecipeIds.map((id) => (
+    <li
+      key={id}
+      onClick={() => {
+        openRecipe(id);
+      }}
+    >
+      <img src="REPLACE ME" width="200" height="200" />
+      <div>recipe title</div>
+    </li>
+  ));
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div id="image list">
+        <div>
+          <ul>{recipes}</ul>
+        </div>
+      </div>
+    </div>
+  );
+}
