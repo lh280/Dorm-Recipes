ba542336e2d3c5f46e51909fb6129f5e1b03443a
JulianGrijalva <jgrijalva@middlebury.edu>
2024-10-31 20:00:44 -0400

    updated knexfiles

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,9 @@
-// knexfile.js
-require('dotenv').config();
+/* eslint-disable import/no-extraneous-dependencies */
+const { loadEnvConfig } = require("@next/env");
 
+// Adapted from NextJS knex example
+const dev = process.env.NODE_ENV !== "production";
+const { DATABASE_URL } = loadEnvConfig("./", dev).combinedEnv;
 
 
 module.exports = {
