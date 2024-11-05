55c3542cbeb633e7b1de82d121a5118a8849b567
= <=>
2024-11-04 23:52:30 -0500

    updated _document.js file

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,19 @@
+import * as React from "react";
 import { Html, Head, Main, NextScript } from "next/document";
+import { DocumentHeadTags, documentGetInitialProps } from "@mui/material-nextjs/v13-pagesRouter";
+import createEmotionCache from "../material/createEmotionCache";
+import theme from "../material/theme";
 
-export default function Document() {
+export default function MyDocument(props) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="emotion-insertion-point" content="" />
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -11,3 +21,10 @@ export default function Document() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const finalProps = await documentGetInitialProps(ctx, {
+    emotionCache: createEmotionCache(),
+  });
+  return finalProps;
+};
