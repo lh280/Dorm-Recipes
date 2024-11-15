22f07132a1d04313805c82ea40482d1ba44be2a8
Col McDermott <cmcdermott@middlebury.edu>
2024-11-15 16:03:47 -0500

    more seed files migrated

--- db/seeds/load_reviews.js
+++ db/seeds/load_reviews.js
@@ -1,13 +1,14 @@
-// /* eslint-disable func-names */
-// /* eslint no-unused-vars: ["error", { "args": "none" }] */
-// const fs = require("fs");
+/* eslint-disable func-names */
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
+const fs = require("fs");
 
-// exports.seed = function (knex) {
-//     const contents = fs.readFileSync("./db/seeds/reviewSeed.json");
-//     const data = JSON.parse(contents);
+exports.seed = function (knex) {
+    const contents = fs.readFileSync("./db/seeds/reviewSeed.json");
+    const data = JSON.parse(contents);
+    console.group(data);
 
-//     // Deletes ALL existing entries
-//     return knex("Reviews")
-//         .del()
-//         .then(() => knex.insert("Reviews", data));
-// };
+    // Deletes ALL existing entries
+    return knex("Reviews")
+        .del()
+        .then(() => knex.batchInsert("Reviews", data));
+};
--- db/seeds/load_users.js
+++ db/seeds/load_users.js
@@ -1,13 +1,13 @@
-// /* eslint-disable func-names */
-// /* eslint no-unused-vars: ["error", { "args": "none" }] */
-// const fs = require("fs");
+/* eslint-disable func-names */
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
+const fs = require("fs");
 
-// exports.seed = function (knex) {
-//     const contents = fs.readFileSync("./db/seed/usersSeed.json");
-//     const data = JSON.parse(contents);
+exports.seed = function (knex) {
+    const contents = fs.readFileSync("./db/seeds/usersSeed.json");
+    const data = JSON.parse(contents);
 
-//     // Deletes ALL existing entries
-//     return knex("Users")
-//         .del()
-//         .then(() => knex.insert("Users", data));
-// };
+    // Deletes ALL existing entries
+    return knex("Users")
+        .del()
+        .then(() => knex.batchInsert("Users", data));
+};
