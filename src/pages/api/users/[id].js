13ce88821c3860520af19c7aa8d8ddad777e89e8
Elise Scott <emscott@middlebury.edu>
2024-11-15 14:26:07 -0500

    added get and put functions for user

---
+++ src/pages/api/users/[id].js
@@ -0,0 +1,34 @@
+import { createRouter } from "next-connect";
+import User from "../../../../models/User";
+import { onError } from "../../../lib/middleware";
+
+const router = createRouter();
+
+router
+  .get(async (req, res) => {
+    const recipe = await User.query()
+      .findById(req.query.id)
+      .withGraphFetched("related")
+      .throwIfNotFound();
+    res.status(200).json(recipe);
+  })
+  .put(async (req, res) => {
+    // PUT endpoint for editing a user's info
+    try {
+      const { id, ...updatedUser } = req.body;
+      if (id !== parseInt(req.query.id, 10)) {
+        res.status(400).end(`URL and object does not match`);
+        return;
+      }
+      const updatedRecord = await User.query().updateAndFetchById(
+        id,
+        updatedUser,
+      );
+      res.status(200).json(updatedRecord);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Failed to update user" });
+    }
+  });
+
+export default router.handler({ onError });
