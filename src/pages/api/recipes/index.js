e0e5cbc8234c2094466aa724050c601913238aed
lh280 <laurenhc123@gmail.com>
2024-11-19 10:31:51 -0500

    ignore eslint camel-case error

--- src/pages/api/recipes/index.js
+++ src/pages/api/recipes/index.js
@@ -6,9 +6,11 @@ const router = createRouter();
 
 router.post(async (req, res) => {
   try {
+    // eslint-disable-next-line 
     const { title, description, prep_time, instructions } = req.body;
 
     // Validate the required fields
+    // eslint-disable-next-line
     if (!title || !description || !prep_time || !instructions) {
       return res.status(400).json({ message: "All fields are required." });
     }
@@ -17,6 +19,7 @@ router.post(async (req, res) => {
     const newRecipe = await Recipe.query().insertAndFetch({
       title,
       description,
+      // eslint-disable-next-line 
       prep_time,
       instructions,
     });
