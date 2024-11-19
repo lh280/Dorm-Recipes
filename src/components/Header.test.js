17c338b83dcd9828935e73ed0ec41394fe801939
= <=>
2024-11-18 23:59:58 -0500

    added user page. Removed weird gradient. Routes arent done but its better than nothing

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -3,13 +3,13 @@ import Header from "./Header"
 
 describe("Header: Testing header funtionality",() => {
     const handler = jest.fn();
-    const currentUser = "Z"
+    const currentUser = {user_id:0,email:"test@gmail.com",created_at:"21 Jan 2024 00:00:00 GMT"}
      beforeEach(() => {
     handler.mockReset();
   });
     test("Header renders both the Site name and a user Icon", () => {
-        const {getByText} = render(<Header setCurrentRecipe={handler} currentUser={currentUser}/>)
+        const {getByText} = render(<Header setCurrentRecipe={handler} currentUser={currentUser} viewAccount={handler}/>)
         expect(getByText("Dorm Recipes")).toBeVisible();
-        expect(getByText(currentUser)).toBeVisible();
+        expect(getByText(currentUser.user_id)).toBeVisible();
     })
 })
