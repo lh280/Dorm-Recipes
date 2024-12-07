875066898312160055e2151aa13170b0188b4415
lh280 <laurenhc123@gmail.com>
2024-12-07 13:44:32 -0500

    update recipe card UI: fixed size cards

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,38 +1,46 @@
-import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
+import Box from '@mui/material/Box';
 import Image from 'next/image';
 import PropTypes from 'prop-types';
 import RecipeShape from './RecipeShape';
 
 export default function Section(props) {
-  const {title, recipes, openRecipe} =  props; // TODO: ASK WHY THIS WORKED AND THE OLD ONE DIDNT
+  const {title, recipes, openRecipe} =  props; // TODO: ASK WHY THIS WORKED AND THE OLD ONE DIDN'T
 
   const cards = [...recipes].map((rec) => (
-    <Card
-      key={rec.id}
-      onClick={() => {openRecipe(rec.recipe_id)}}
-      variant="outlined"
-    >
-      <CardActionArea>
-        <CardContent>
-          <Image src={(rec.img ? rec.img : "/food.jpg")} width={200} height={200} alt="Picture of the recipe"/>
-
-          <Typography textAlign="center" variant="h6">{rec.title}</Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    
+    <Grid item xs={12} sm={6} md={3} key={rec.id} sx={{ display: "flex", justifyContent: "center" }}>
+      <Card
+        key={rec.id}
+        onClick={() => {openRecipe(rec.recipe_id)}}
+        variant="outlined"
+        data-testid="recipe"
+        sx={{ maxWidth: 300, width: "100%" }}
+      >
+        <CardActionArea>
+          <CardContent>
+            <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", mb: 2 }}>
+              <Image src={(rec.img ? rec.img : "/food.jpg")} width={200} height={200} alt="Picture of the recipe"/>
+            </Box>
+            <Typography textAlign="center" variant="h6">{rec.title}</Typography>
+            {/* TODO: same as RecipesView */}
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Grid>
   ));
 
   return (
     <div>
       <Typography variant="h2">{title}</Typography>
       <div id="image list">
-        <Stack direction="row" spacing={2}> 
+        <Grid container spacing={2}> 
           {cards}
-        </Stack>
+        </Grid>
       </div>
     </div>
   );
