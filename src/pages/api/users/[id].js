cfcdccb76d7019c38ce83b503a153aaa6e4dfcb5
JulianGrijalva <jgrijalva@middlebury.edu>
2024-12-10 18:53:48 -0500

    fixed bugs and used correct fetch method

--- src/pages/api/users/[id].js
+++ src/pages/api/users/[id].js
@@ -12,12 +12,7 @@ router
     try{
     const user = await User.query()
       .where('user_id', userID)
-      .withGraphFetched("[user_recipes, pantry_items, reviews.[recipe(onlyTitle)]]")
-      .modifyEager('reviews', (builder) => {
-          builder
-              .leftJoin('recipes', 'reviews.recipeId', 'recipes.id')
-              .select('reviews.*', 'recipes.title as recipeTitle');
-      })
+      .withGraphFetched('[user_recipes, pantry_items, user_reviews.[recipes(onlyTitle)]]') // Changed reviews to user_reviews
       .first()
       .throwIfNotFound();
     if (!user) {
